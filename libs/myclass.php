dd807124958c73b4e96293334a61fa71e9e7b8ff
Fırat Işıldak <firatisildak1@gmail.com>
2024-05-31 12:42:35 +0300

    login_index and myclass pages added

---
+++ libs/myclass.php
@@ -0,0 +1,85 @@
+<?php
+class sqlProcess
+{
+    function __construct()
+    {
+        try {
+            //PDO sınıfının constructor'ında, veritabanı sunucusunun adresi (localhost), veritabanı adı (db), karakter seti (utf8), kullanıcı adı (root) ve şifre ('') gibi bilgiler yer alır. Bu bilgiler, veritabanı sunucusuna bağlanmak için gereklidir.
+            $db = new PDO("mysql:host=localhost; dbname=db; charset=utf8", 'root', '');
+            //echo "veri tabanı bağlantısı başarılı";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    function siralammodal_ForDelete($exampleModal, $butonName)
+    {
+    echo '<div class="modal fade" id="' . $exampleModal . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">UYARI..!</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Silmek istediğinize emin misiniz?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="submit-btn bg-danger inputOval" data-bs-dismiss="modal">Kapat</button>
+                        <button type="submit" name="' . $butonName . '" class="submit-btn bg-danger inputOval">Sil</button>
+                    </div>
+                </div>
+            </div>
+        </div>';
+    }
+    function loginControl($post, $query, $name, $pass, $who)
+    {
+        if (isset($_POST[$post])) {
+            $username = $_POST[$name];
+            $password = $_POST[$pass];
+
+            global $db;
+            // Assuming $db is your database connection object
+            $kullanici_sor = $db->prepare($query);
+            $kullanici_sor->execute([$username, $password]);
+            $say = $kullanici_sor->rowCount();
+
+            if ($say == 1) {
+                $_SESSION["LoggedIn"] = true;
+                $_SESSION["username"] = $username;
+                if ($who == 'admin') {
+                } else {
+                    goAndComeBack("index.php", 2, 1);
+                }
+            } else {
+                $_SESSION["LoggedIn"] = false;
+                echo '<script>alert("Kullanıcı adı veya şifre hatalı.");</script>';
+                goAndComeBack(0, 0.1);
+                exit;
+            }
+        }
+    }
+    function sqlsorgu($requestQuery, $query, $params, $choos = 0, $query2, $params2)
+    {
+        if (isset($_POST[$requestQuery])) {
+            global $db, $params, $kayitSayisi;
+            if ($choos != 0) {
+                //alttaki 3 satır ile verilen params2 ile verilen verinin kaçtane olduğunu sorgu ile aratıyor.
+                $sorgu = $db->prepare($query2);
+                $guncelle = $sorgu->execute(($params2));
+                $kayitSayisi = $sorgu->fetchColumn();
+                if ($kayitSayisi == 0) {
+                    $sorgu = $db->prepare($query);
+                    $guncelle = $sorgu->execute(($params));
+                }
+            } else {
+                $sorgu = $db->prepare($query);
+                $guncelle = $sorgu->execute(($params));
+            }
+            if ($guncelle) {
+                echo '<script>alert("işlem Başarılı.");</script>';
+            } else {
+                echo '<script>alert("Bir hata oluştu, tekrar kontrol edin.");</script>';
+            }
+        }
+    }
+}
---
+++ login_index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<?php
+	include("MyClass.php");
+	include("functions/routing.php");
+	include("inc/database_Connection.php"); //bu kod bizim veritabaı ile bağlantımızı sağlar.
+	$userProcess = new sqlProcess();
+	if (isset($_POST['kayit'])) {
+		//alttaki kod parçası bizim kullanıcımızın kayıt işlemi için kullanılır.
+		$username = $_POST['username'];
+		$mail = $_POST["mail"];
+		$password = $_POST['password'];
+		$params = [$username, $mail, $password];
+		$params2 = [$username];
+		$userProcess->sqlsorgu('kayit', 'INSERT INTO login (name, mail, password) VALUES (?, ?, ?)', $params, 1, 'SELECT COUNT(*) FROM login WHERE name = ?', $params2);
+	}
+	$userProcess->loginControl('giris', 'SELECT * FROM login WHERE name=? && password=?', 'name', 'pass', 'user');//giris=kayıt butonun ismi.
+	?>
+	<link rel="stylesheet" href="./css/login_style.css">
+	<script src="https://kit.fontawesome.com/0761d8fd00.js" crossorigin="anonymous"></script>
+	<script src="https://unpkg.com/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
+</head>
+
+<body>
+	<div class="main">
+		<div class="form-box">
+			<div class="button-box">
+				<div id="btn"></div><!--bu divi kullanmamın sebebi butonlar ile sayfalar arası geçişi sağlamak.-->
+				<button type="button" class="toggle-btn" onclick="login()">Giriş</button>
+				<button type="button" class="toggle-btn" onclick="register()">Kayıt Ol</button>
+			</div>
+			<div class="social-icons">
+				<a href="#"><i class="fa-brands fa-facebook social mb-4"></i></a>
+				<a href="https://www.linkedin.com/in/f%C4%B1rat-i%C5%9F%C4%B1ldak-608176250/"><i class="fa-brands fa-linkedin social  mb-4"></i></a>
+				<a href="#"><i class="fa-brands fa-github social  mb-4"></i></a>
+			</div>
+			<form id="Login" class="input-group" action="login_index.php" method="post"><!--method="post" eğer bu kısmı böyle vermezsem veritabanı bağlantısında post özelliğine kod yazamam.-->
+				<input type="text" name="name" class="input-field" placeholder="Kullanıcı Adı" required><!--required kodu boş geçilemez anlamına geliyor.-->
+				<input type="password" name="pass" class="input-field" placeholder="Şifre Gir" required>
+				<a href="#">Şifremi Unuttum</a>
+				<button type="submit" name="giris" class="submit-btn">Giriş</button><!--burada verdiğimiz çok önemli bu ismi veri tabanı bağlantısında giris butonunun tıklanma özelliğine yazıyoruz.-->
+			</form>
+			<form id="Register" class="input-group" action="login_index.php" method="post"><!--Yani, bu kodda "sql_islem.php" değeri, form verilerinin sunucuda işlenmesi için yönlendirilecek olan betik dosyasını belirtir. Bu dosya, kullanıcının girdiği form verilerini alacak, işleyecek ve veritabanı işlemleri gibi gerekli işlemleri gerçekleştirecektir.-->
+				<input type="text" name="username" class="input-field" placeholder="Kullanıcı Adı" required>
+				<input type="password" name="password" class="input-field" placeholder="Şifre Gir" required>
+				<input type="text" name="mail" class="input-field" placeholder="Mail Gir" required>
+				<button type="submit" name="kayit" class="submit-btn">Kayıt Ol</button>
+			</form>
+		</div>
+	</div>
+	<script>
+		var x = document.getElementById("Login");
+		var y = document.getElementById("Register");
+		var z = document.getElementById("btn");
+
+		function register() {
+			x.style.left = "-400px";
+			y.style.left = "50px";
+			z.style.left = "110px";
+		}
+
+		function login() {
+			x.style.left = "50px";
+			y.style.left = "450px";
+			z.style.left = "0px";
+		}
+	</script>
+</body>
+
+</html>
